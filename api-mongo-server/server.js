ee24a48cbfd213bd19472ef5333570e57e17b589
Pratik <baniyapratik@gmail.com>
2018-04-01 23:53:03 -0700

    changed the pkg's

--- api-mongo-server/server.js
+++ api-mongo-server/server.js
@@ -30,7 +30,17 @@ require('./routes/authRoutes')(app);
 require('./routes/userRoutes')(app);
 require('./routes/projectRoutes')(app);
 require('./routes/bidRoutes')(app);
-
+if (process.env.NODE_ENV === 'production') {
+  //Express will serve up production assets
+  //like our main.js file, or main.css file
+  app.use(express.static('../build'));
+  //Express will serve up the index.html file
+  //if it doesnt recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.listen(PORT, () => {
   console.log(`Listening on port`, PORT);
 });
